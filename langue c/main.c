9a65ef5a18f6c1cdd392173dc7f83a992f48df58
Julien 安 <an@yasumatoMacBook-Pro.local>
2015-01-07 16:39:34 +0800

    Initial Commit

---
+++ langue c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  langue c
+//
+//  Created by Julien 安 on 15/1/7.
+//  Copyright (c) 2015年 Julien 安. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
